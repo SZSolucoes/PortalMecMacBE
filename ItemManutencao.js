632a0036642d67fd922ccdafaf38414d382945bf
roneymaia <roneymaia@gmail.com>
2019-02-01 17:02:32 -0200

    Conclusão de melhorias

--- ItemManutencao.js
+++ ItemManutencao.js
@@ -226,20 +226,63 @@ module.exports = (app, mysqlCon, sockets, axios, _) => {
     
         con.end();
     });
+
+    app.get('/itemmanutxvehiclevtwo', (req, res) => {
+        const con = mysqlCon();
+        const params = req.query
+
+        if (params.id) {
+            let query = 'SELECT * FROM itemmanutencao INNER JOIN itemmanutxvehicle ON '
+            query += 'itemmanutencao.id = itemmanutxvehicle.itemmanutid '
+            query += 'WHERE itemmanutxvehicle.vehicleid = ? ORDER BY itemmanutencao.id';
+            try {
+                con.connect();
+                con.query(query, [params.id], (error, results, fields) => {
+                    if (!error) {
+                        if (!results) return [];
+
+                        res.send(results);
+                    } else {
+                        res.send([]);
+                    }
+                });
+            } catch (e) {
+                console.log(e);
+                res.send([]);
+            }
+        } else {
+            res.send([]);
+        }
+
+        con.end();
+    });
     
     app.post('/itemmanutxvehicle', (req, res) => {
         const con = mysqlCon();
-        const jsonRes = { success: 'true', message: 'Inclusão efetuada com sucesso!' };
+        const jsonRes = { success: 'true', message: 'Vínculo efetuado com sucesso!' };
         const params = req.body;
 
         if (params && 
-            params.itemmanutID && 
-            params.vehiclesTo && 
-            params.vehiclesTo instanceof Array && 
-            params.vehiclesTo.length) 
+            params.itens && 
+            params.veiculos && 
+            params.itens instanceof Array && 
+            params.itens.length &&
+            params.veiculos instanceof Array && 
+            params.veiculos.length) 
         {
             try {
-                const mappedItens = _.map(params.vehiclesTo, (item) => ([ params.itemmanutID, item ]));
+                const mappedItens = [];
+
+                for (let index = 0; index < params.veiculos.length; index++) {
+                    const veiculo = params.veiculos[index];
+
+                    for (let indexB = 0; indexB < params.itens.length; indexB++) {
+                        const item = params.itens[indexB];
+
+                        mappedItens.push([item, veiculo]);
+                    }
+                    
+                }
 
                 con.connect();
                 con.query('INSERT IGNORE INTO itemmanutxvehicle (itemmanutid, vehicleid) VALUES ?', [mappedItens], (error, results, fields) => {
@@ -247,13 +290,6 @@ module.exports = (app, mysqlCon, sockets, axios, _) => {
                         jsonRes.success = 'false';
                         jsonRes.message = error.sqlMessage;
                         console.log(error);
-                    } else {
-                        sockets.forEach((socket) => 
-                            socket.emit(
-                                'table_itemmanutxvehicle_changed', 
-                                params.itemmanutID
-                            )
-                        );
                     }
                     res.send(jsonRes);
                 });
@@ -383,10 +419,11 @@ module.exports = (app, mysqlCon, sockets, axios, _) => {
 
         try {
             con.connect();
-            con.query(`DELETE FROM itemmanutxvehicle WHERE id = ?`, [params.id], (error, results, fields) => {
+            con.query(`DELETE FROM itemmanutxvehicle WHERE id IN (?)`, [params.itens], (error, results, fields) => {
                 if (error) {
                     jsonRes.success = 'false';
                     jsonRes.message = error.sqlMessage;
+                    console.log(error);
                 } else {
                     sockets.forEach((socket) => 
                         socket.emit(
